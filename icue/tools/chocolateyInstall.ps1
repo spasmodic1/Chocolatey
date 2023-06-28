3430b93b1d6d46312a2fe5b91facf87f28780096
ttrifaro <ttrifaro@cvent.com>
2023-06-28 01:22:04 -0400

    first commit

---
+++ icue/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  url           = 'https://downloads.corsair.com/Files/CUE/iCUESetup_4.33.138_release.msi'
+  checksum      = '67331160822554f20342c693f27b7e998bdf9bfb74df1b0f5acb8510e05fe206'
+  checksumType  = 'SHA256'
+
+  softwareName  = 'icue*'
+
+  silentArgs    = "/QN"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
